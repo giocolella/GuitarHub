918133d215d5529983177e4a30ed235d8022c175
Giorgio Colella <giorgio.colella.gc@gmail.com>
2023-07-16 22:17:50 +0200

    New version
    Fatti:
    - aggiunte funzionalità admin come modifica,aggiunta e rimozione prodotto
    Da fare:
    -migliorare gestione quantità carrello

---
+++ guitarhub3.2/GuitarHub/src/main/java/it/guitarhub/controller/LoginServlet.java
@@ -0,0 +1,60 @@
+package it.guitarhub.controller;
+
+import java.io.IOException;
+import java.sql.SQLException;
+
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import it.guitarhub.beans.User;
+import it.guitarhub.model.UserDAO;
+
+
+@WebServlet("/Login")
+public class LoginServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    public LoginServlet() {
+        super();
+    }
+	
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String email = request.getParameter("emailLog");
+		String password = request.getParameter("passLog");
+		
+		UserDAO db = new UserDAO();
+		
+		User user = new User();
+		try {
+			user = db.doRetrieveByEmailLogin(email);
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		
+		if(user == null) {
+			request.setAttribute("errors", "Credenziali non valide!");
+			request.getRequestDispatcher("login.jsp").forward(request, response);
+			return;
+		} else if (!user.getPassword().equals(password)) {
+			request.setAttribute("errors", "Credenziali non valide!");
+			request.getRequestDispatcher("login.jsp").forward(request, response);
+			return;
+		} else if ("admin".equals(user.getRole())) {
+			request.getSession().setAttribute("isAdmin", true);
+			response.sendRedirect(request.getContextPath() + "/views/admin/adminSearch.jsp");
+		} else {
+			request.getSession().setAttribute("User", user);
+			response.sendRedirect(request.getContextPath() + "/views/home.jsp");
+		}
+	}
+
+	
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doGet(request, response);
+	}
+
+}
---
+++ guitarhub3.2/GuitarHub/src/main/java/it/guitarhub/model/ImplementDAO.java
@@ -0,0 +1,18 @@
+package it.guitarhub.model;
+
+import java.sql.SQLException;
+import java.util.Collection;
+
+public interface ImplementDAO<T> {
+
+	Collection<T> doRetrieveAll(String order) throws SQLException;
+
+	T doRetriveByKey(int code) throws SQLException;
+
+	void doSave(T dao) throws SQLException;
+
+	int doUpdate(T dao) throws SQLException;
+
+	boolean doDelete(int code) throws SQLException;
+
+}
